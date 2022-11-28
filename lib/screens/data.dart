7c62c34a946008496e3fa2207c93f4e5abec91f5
freddy2303 <freddyzoecorrea@gmail.com>
2022-11-27 20:59:36 -0500

    cambios

--- lib/screens/data.dart
+++ lib/screens/data.dart
@@ -3,34 +3,34 @@ import 'package:sesion_09/screens/product_model.dart';
 class Data {
   static List<ProductModel> generateProducts() {
     return [
-      ProductModel(1, "assets/ropa10.png", "¡OFERTA!",
+      ProductModel(1, "assets/varon5.png", "¡OFERTA!",
           "Pantalon recto\n con cremallera", "men shoes", 99.90),
       ProductModel(
           2, "assets/ropa11.png","¡OFERTA!", "Vestido sudadera/cuello redondo", "men shoes", 137.56),
       ProductModel(
           3, "assets/ropa12.png", "¡OFERTA!", "4 piezas Jersey cuello alto", "men shoes", 155.99),
       ProductModel(
-          4, "assets/ropa13.png", "¡OFERTA!", "Vestido de manga farol con nudo", "men shoes", 70.99),
+          4, "assets/varon4.png", "¡OFERTA!", "Vestido de manga farol con nudo", "men shoes", 70.99),
       ProductModel(
-          5, "assets/ropa14.png", "¡OFERTA!", "Cazadora con estampado floral en contraste", "men shoes", 212.56),
+          5, "assets/ropa14.png", "¡OFERTA!", "Cazadora con estampado floral en contraste", "men shoes", 250),
       ProductModel(
-          6, "assets/ropa15.png","¡OFERTA!", "Vestido con cinturon y estampado geometrico", "men shoes", 212.56),
+          6, "assets/varon3.png","¡OFERTA!", "Vestido con cinturon y estampado geometrico", "men shoes", 99),
       ProductModel(
-          7, "assets/ropa16.png", "¡OFERTA!","Top tubo corto", "men shoes", 50.99),
+          7, "assets/varon1.png", "¡OFERTA!","Top tubo corto", "men shoes", 50.99),
       ProductModel(
           8, "assets/ropa17.png","¡OFERTA!", "Top de tirantes cortos", "men shoes", 212.56),
       ProductModel(
-          9, "assets/ropa18.png", "¡OFERTA!", "Vestido tirantes, escote corazon", "men shoes", 212.56),
+          9, "assets/ropa18.png", "¡OFERTA!", "Vestido tirantes, escote corazon", "men shoes", 180),
       ProductModel(
-          10, "assets/ropa19.png", "¡OFERTA!","Top corto de cuello scoop unicolor", "men shoes", 212.56),
+          10, "assets/ropa19.png", "¡OFERTA!","Top corto de cuello scoop unicolor", "men shoes", 60.5),
       ProductModel(
-          11, "assets/ropa19.png", "¡OFERTA!", "Men's Shoes", "men shoes", 212.56),
+          11, "assets/varon2.png", "¡OFERTA!", "Men's Shoes", "men shoes", 80.5),
       ProductModel(
-          12, "assets/ropa19.png","¡OFERTA!", "Men's Shoes", "men shoes", 212.56),
+          12, "assets/ropa15.png","¡OFERTA!", "Men's Shoes", "men shoes", 200.99),
       ProductModel(
-          13, "assets/ropa19.png", "¡OFERTA!", "Men's Shoes", "men shoes", 212.56),
+          13, "assets/ropa13.png", "¡OFERTA!", "Men's Shoes", "men shoes", 155.6),
       ProductModel(
-          14, "assets/ropa19.png", "¡OFERTA!", "Men's Shoes", "men shoes", 212.56),
+          14, "assets/ropa10.png", "¡OFERTA!", "Men's Shoes", "men shoes", 99.99),
     ];
   }
 
--- lib/screens/homescreens.dart
+++ lib/screens/homescreens.dart
@@ -50,235 +50,260 @@ class Homescreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: (Preferences.genero != 1)
-              ? Color.fromARGB(95, 214, 145, 218)
-              : Color.fromARGB(255, 251, 249, 250),
-          title: const Text(
-            'Shine Clothes',
-            style: TextStyle(
-                color: Colors.black, fontSize: 30, fontWeight: FontWeight.bold),
-          ),
-          centerTitle: true,
-          actions: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.notifications_none),
-              onPressed: () {},
-            ),
-            IconButton(
-              icon: const Icon(Icons.shopping_cart_outlined),
-              onPressed: () {},
-            )
-          ],
-          elevation: 0,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+      appBar: AppBar(
+        backgroundColor: (Preferences.genero != 1)
+            ? Color.fromARGB(95, 214, 145, 218)
+            : Color.fromARGB(255, 251, 249, 250),
+        title: const Text(
+          'Shine Clothes',
+          style: TextStyle(
+              color: Colors.black, fontSize: 30, fontWeight: FontWeight.bold),
         ),
-        drawer: const CustomWidgetHome(),
-        body: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("assets/fondo2.webp"), fit: BoxFit.cover),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.notifications_none),
+            onPressed: () {},
           ),
-          child: ListView(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(3),
-                child: Stack(
-                  children: [
-                    ClipRRect(
-                      child: Image.asset("assets/banner8.png"),
+          IconButton(
+            icon: const Icon(Icons.shopping_cart_outlined),
+            onPressed: () {},
+          )
+        ],
+        elevation: 0,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+      ),
+      drawer: const CustomWidgetHome(),
+      body: Container(
+        // decoration: const BoxDecoration(
+        //   image: DecorationImage(
+        //       image: AssetImage("assets/fondo2.webp"), fit: BoxFit.cover),
+        // ),
+        child: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(3),
+              child: Stack(
+                children: [
+                  ClipRRect(
+                    child: Image.asset("assets/banner8.png"),
 
-                      // borderRadius: BorderRadius.circular(10),
+                    // borderRadius: BorderRadius.circular(10),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(15),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
+            ),
 
-              const SizedBox(
-                height: 5,
+            const SizedBox(
+              height: 5,
+            ),
+            // SizedBox(
+            //   height: 100,
+            //   child: ListView(
+            //     scrollDirection: Axis.horizontal,
+            //     children: builCategories(),
+            //   ),
+
+            // ),
+            ClipRRect(
+              child: Image.asset("assets/banner1.2.png"),
+
+              // borderRadius: BorderRadius.circular(10),
+            ),
+            SizedBox(height: 5),
+            ClipRRect(
+              child: Image.asset("assets/banner1.3.png"),
+
+              // borderRadius: BorderRadius.circular(10),
+            ),
+            SizedBox(height: 5),
+            ClipRRect(
+              child: Image.asset("assets/banner1.4.png"),
+
+              // borderRadius: BorderRadius.circular(10),
+            ),
+            SizedBox(height: 15),
+            //  SizedBox(
+            //   height: 100,
+            //   child: ListView(
+            //     scrollDirection: Axis.horizontal,
+            //     children: builCategories(),
+            //   ),
+
+            // ),
+
+            const SizedBox(
+              height: 10,
+            ),
+            
+          
+             const SizedBox(
+              height: 10,
+            ),
+            ClipRRect(
+              // ignore: sort_child_properties_last
+              child: Image.asset("assets/banner.6.png"),
+              // borderRadius: BorderRadius.circular(10),
               ),
-              // SizedBox(
-              //   height: 100,
-              //   child: ListView(
-              //     scrollDirection: Axis.horizontal,
-              //     children: builCategories(),
-              //   ),
+    
+            const SizedBox(height: 30),
 
-              // ),
-              ClipRRect(
-                child: Image.asset("assets/banner1.2.png"),
-
-                // borderRadius: BorderRadius.circular(10),
+            SizedBox(
+              height: 150,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: builCategories(),
               ),
-              SizedBox(height: 5),
-              ClipRRect(
-                child: Image.asset("assets/banner1.3.png"),
+            ),
+               const SizedBox(height: 30),
 
-                // borderRadius: BorderRadius.circular(10),
-              ),
-              SizedBox(height: 5),
-              ClipRRect(
-                child: Image.asset("assets/banner1.4.png"),
-
-                // borderRadius: BorderRadius.circular(10),
-              ),
-              SizedBox(height: 15),
-              //  SizedBox(
-              //   height: 100,
-              //   child: ListView(
-              //     scrollDirection: Axis.horizontal,
-              //     children: builCategories(),
-              //   ),
-
-              // ),
-
-              const SizedBox(
-                height: 10,
-              ),
-              // Padding(
-              //   padding: const EdgeInsets.symmetric(horizontal: 10),
-              //   child: RichText(
-              //     textAlign: TextAlign.center,
-              //     text: const TextSpan(
-              //         text: "Galeria de ofertas",
-              //         style: TextStyle(
-
-              //             color: Color.fromARGB(255, 232, 13, 13),
-              //             fontSize: 20,
-              //             fontWeight: FontWeight.bold)),
-              //   ),
-              // ),
-              ClipRRect(
-                child: Image.asset("assets/banner.6.png"),
-                borderRadius: BorderRadius.circular(50),
-
-                // borderRadius: BorderRadius.circular(10),
-              ),
-              SizedBox(height: 30),
-
-              SizedBox(
-                height: 150,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: builCategories(),
-                ),
-              ),
-
-              const SizedBox(height: 20),
               Padding(
-                padding: const EdgeInsets.all(8),
-                child: GridView.count(
-                  crossAxisCount: 2,
-                  childAspectRatio: 0.7,
-                  shrinkWrap: true,
-                  physics: const ClampingScrollPhysics(),
-                  padding: const EdgeInsets.all(0),
-                  children: Data.generateProducts()
-                      .map(
-                        (e) => SizedBox(
-                          // shape: BeveledRectangleBorder(
-                          //     borderRadius: BorderRadius.circular(15),
-                          //     ),
-                          //     elevation: 0,
-                          child: InkWell(
-                            onTap: () {},
-                            child: Container(
-                              margin: const EdgeInsets.only(
-                                  left: 15, right: 15, top: 5),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Image.asset(e.image, height: 200, width: 250),
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: RichText(
+                textAlign: TextAlign.start,
+                text: const TextSpan(
+                    text: "Ofertas especiales",
+                    style: TextStyle(
 
-                                  // RichText(textAlign: TextAlign.start,
-                                  // text: TextSpan(text: e.title,
-                                  // style: const TextStyle(
-                                  //   color: Colors.red,
-                                  //   fontSize: 15,
-                                  // )
+                        color: Color.fromARGB(255, 231, 128, 49),
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold)),
+              ),
+            ),
 
-                                  // ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: GridView.count(
+                crossAxisCount: 2,
+                childAspectRatio: 0.7,
+                shrinkWrap: true,
+                physics: const ClampingScrollPhysics(),
+                padding: const EdgeInsets.all(0),
+                children: Data.generateProducts()
+                    .map(
+                      (e) => SizedBox(
+                        // shape: BeveledRectangleBorder(
+                        //     borderRadius: BorderRadius.circular(15),
+                        //     ),
+                        //     elevation: 0,
+                        child: InkWell(
+                          onTap: () {},
+                          child: Container(
+                            margin: const EdgeInsets.only(
+                                left: 15, right: 15, top: 5),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Image.asset(e.image, height: 200, width: 250),
 
-                                  // ),
-                                  //  const SizedBox(height: 2,),
-                                  // RichText(textAlign: TextAlign.start,
-                                  // text: TextSpan(text: e.type,
-                                  // style: const TextStyle(
-                                  //   color: Color.fromARGB(215, 132, 75, 75), fontWeight: FontWeight.bold,
-                                  //   fontSize: 13,
-                                  // )
+                                RichText(textAlign: TextAlign.center,
+                                text: TextSpan(text: e.title,
+                                style: const TextStyle(
+                                  color: Colors.red,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold
+                                )
 
-                                  // ),
+                                ),
 
-                                  // ),
+                                ),
+                                //  const SizedBox(height: 2,),
+                                // RichText(textAlign: TextAlign.start,
+                                // text: TextSpan(text: e.type,
+                                // style: const TextStyle(
+                                //   color: Color.fromARGB(215, 132, 75, 75), fontWeight: FontWeight.bold,
+                                //   fontSize: 13,
+                                // )
 
-                                  Container(
-                                    color: Colors.purple,
-                                    child: Row(
-                                      children: [
-                                        Padding(
-                                          padding: const EdgeInsets.all(5),
-                                          child: RichText(
-                                            textAlign: TextAlign.start,
-                                            text: TextSpan(
-                                                text: "\$ ${e.price}",
-                                                style: const TextStyle(
-                                                    color: Color.fromARGB(
-                                                        255, 253, 249, 249),
-                                                    fontSize: 15,
-                                                    fontWeight:
-                                                        FontWeight.bold)),
-                                          ),
+                                // ),
+
+                                // ),
+
+                                Container(
+                                  color: Color.fromARGB(255, 244, 100, 148),
+                                  child: Row(
+                                    children: [
+                                      Padding(
+                                        padding: const EdgeInsets.all(5),
+                                        child: RichText(
+                                          textAlign: TextAlign.start,
+                                          text: TextSpan(
+                                              text: "\$ ${e.price}",
+                                              style: const TextStyle(
+                                                  color: Color.fromARGB(
+                                                      255, 253, 249, 249),
+                                                  fontSize: 15,
+                                                  fontWeight: FontWeight.bold)),
                                         ),
-                                        IconButton(
-                                            onPressed: () {
-                                              Navigator.pushNamed(context,
-                                                  MyRoutes.rFavoritePage);
-                                            },
-                                            icon: const Icon(
-                                              Icons.favorite_border_outlined,
-                                              color: Color.fromARGB(
-                                                  255, 235, 27, 31),
-                                            )),
-                                        InkWell(
-                                            onTap: () {},
-                                            child: const Icon(
-                                              Icons.shopping_cart_outlined,
-                                              color: Colors.black,
-                                            )),
-                                      ],
-                                    ),
-                                  )
+                                      ),
+                                      IconButton(
+                                          onPressed: () {
+                                            Navigator.pushNamed(context,
+                                                MyRoutes.rFavoritePage);
+                                          },
+                                          icon: const Icon(
+                                            Icons.favorite_border_outlined,
+                                            color: Color.fromARGB(
+                                                255, 235, 27, 31),
+                                          )),
+                                      InkWell(
+                                          onTap: () {},
+                                          child: const Icon(
+                                            Icons.shopping_cart_outlined,
+                                            color: Colors.black,
+                                          )),
+                                    ],
+                                  ),
+                                )
 
-                                  //     ElevatedButton(onPressed: (){
+                                //     ElevatedButton(onPressed: (){
 
-                                  // //     // },
-                                  //     // //  style: ButtonStyle(
-                                  //     // //   backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                                  //     // //   shape: MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)))
-                                  //     // //  ),
-                                  //     //  child: const Icon(Icons.shopping_cart_outlined,color:Colors.black),
+                                // //     // },
+                                //     // //  style: ButtonStyle(
+                                //     // //   backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                                //     // //   shape: MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)))
+                                //     // //  ),
+                                //     //  child: const Icon(Icons.shopping_cart_outlined,color:Colors.black),
 
-                                  //     //  )
-                                  //   ],
-                                  // )
-                                ],
-                              ),
+                                //     //  )
+                                //   ],
+                                // )
+                              ],
                             ),
                           ),
                         ),
-                      )
-                      .toList(),
-                ),
-              )
-            ],
-          ),
-        ));
+                      ),
+                    )
+                    .toList(),
+              ),
+
+            ),
+            const Divider(
+                      height:5,
+                      thickness:3,
+                      color:Color.fromARGB(255, 188, 175, 175),),
+             SizedBox(height: 10), 
+             
+
+
+
+            ClipRRect(
+              child: Image.asset("assets/tarjeta.png"),
+
+              // borderRadius: BorderRadius.circular(10),
+            ),
+            
+          ],
+        ),
+        
+      ),
+    );
   }
 }
